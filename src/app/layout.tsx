54baac28435fa84dad71f5f6f7e8e4c7eea92c34
Mihir Manas Mishra <mihirmanas34@gmail.com>
2026-06-29 14:27:31 +0530

    Add cache-busting version parameter to favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml" />
         
         {/* Microsoft Clarity */}
         <Script id="ms-clarity" strategy="afterInteractive">
