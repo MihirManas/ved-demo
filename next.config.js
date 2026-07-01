095a19ff766772f3506862cefb2eda86effb8761
Mihir Manas Mishra <mihirmanas34@gmail.com>
2026-07-01 22:13:41 +0530

    Allow UploadThing domains in Next.js Image component config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,18 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
